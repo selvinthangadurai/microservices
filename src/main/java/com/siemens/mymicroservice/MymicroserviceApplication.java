0e42287930ae3203e5b3e289a84a59e6ab3cd804
selvinthangadurai <selvinm@gmail.com>
2019-07-30 05:19:54 -0700

    my first microservice app

---
+++ src/main/java/com/siemens/mymicroservice/MymicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.mymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymicroserviceApplication.class, args);
+	}
+
+}
